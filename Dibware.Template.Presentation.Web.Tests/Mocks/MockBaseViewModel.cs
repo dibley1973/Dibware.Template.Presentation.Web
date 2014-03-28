9b98195eb0d6bc7988a4a12da7d2766d73ae6c2f
dibley1973 <dibley1973@yahoo.co.uk>
2014-03-28 06:25:54 +0000

    20140328-0625-DWDT Added BaseViewModel tests

---
+++ Dibware.Template.Presentation.Web.Tests/Mocks/MockBaseViewModel.cs
@@ -0,0 +1,8 @@
+﻿using Dibware.Template.Presentation.Web.Models.Base;
+
+namespace Dibware.Template.Presentation.Web.Tests.Mocks
+{
+    internal class MockBaseViewModel : BaseViewModel
+    {
+    }
+}
---
+++ Dibware.Template.Presentation.Web.Tests/Models/Base/BaseViewModelTests.cs
@@ -0,0 +1,53 @@
+﻿using Dibware.Template.Presentation.Web.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Dibware.Template.Presentation.Web.Tests.Models.Base
+{
+    [TestClass]
+    public class BaseViewModelTests
+    {
+        [TestMethod]
+        public void Test_CustomColourTheme_ReturnsSetValue()
+        {
+            // Arrange
+            var model = new MockBaseViewModel();
+            const String expectedValue = "Green";
+
+            // Act
+            model.CustomColourTheme = expectedValue;
+
+            // Assert
+            Assert.AreEqual(expectedValue, model.CustomColourTheme);
+        }
+
+        [TestMethod]
+        public void Test_HasCustomColourTheme_ReturnsFalse_WhenCustomColourThemeNotSet()
+        {
+            // Arrange
+            var model = new MockBaseViewModel();
+            const Boolean expectedValue = false;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(expectedValue, model.HasCustomColourTheme);
+
+        }
+
+        [TestMethod]
+        public void Test_HasCustomColourTheme_ReturnsTrue_WhenCustomColourThemeIsSet()
+        {
+            // Arrange
+            var model = new MockBaseViewModel();
+            const String customColourTheme = "Green";
+            const Boolean expectedValue = true;
+
+            // Act
+            model.CustomColourTheme = customColourTheme;
+
+            // Assert
+            Assert.AreEqual(expectedValue, model.HasCustomColourTheme);
+        }
+    }
+}
